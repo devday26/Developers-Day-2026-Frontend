975d4134885714ec4a2d014dc52315a8f1695a96
Asfand0007 <asfand.khanzada0007@gmail.com>
2026-03-12 10:27:42 +0500

    Commented out map

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <Hero />
       <AboutUs />
       <ModuleCategories />
-      <CampusReservationMap />
+      {/* <CampusReservationMap /> */}
       <ReconLogs />
       <Testimonials />
       <RegistrationBanner />
